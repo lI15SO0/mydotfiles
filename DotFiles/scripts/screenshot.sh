4b3abf65ca433860a6548adec9256fb253a96a79
lI15SO0 <svvivi@outlook.com>
2022-06-03 18:45:46 +0800

    2022-06-03, CHANGE: Remove rime dotfile; CHAGE: remove aria2 dotfile; CHANGE: nvim polybar alacritty

--- DotFiles/scripts/screenshot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-snapdata='%Y%m%d_%H%M%S'
-
-scrot -b -m ~/Pictures/ScreenShot/ScreenShot-$snapdata.png
--- DotFiles/start-up-scripts/auto_switch_bg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-feh --randomize --bg-scale ~/Pictures/WallPapers/*
